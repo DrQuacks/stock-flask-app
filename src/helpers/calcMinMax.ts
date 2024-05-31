7eda3d501be200f795859172c8fd4410faa76d65
Michael Kellar <michael.d.kellar@gmail.com>
2024-05-30 22:19:50 -0700

    update to esNext in tsconfig

--- src/helpers/calcMinMax.ts
+++ src/helpers/calcMinMax.ts
@@ -2,10 +2,15 @@ import * as d3 from "d3";
 import { PlotData } from "src/static/initialPlotState";
 
 const calcMinMax = (stockArray:PlotData) => {
-  const yRange:[number,number] = d3.extent(stockArray.reduce<number[]>((acc,element) => {
+  const extentList = stockArray.reduce<number[]>((acc,element) => {
     const thisExtent = [element.min,element.max]
     return [...acc,...thisExtent]
-  },[]))
+  },[])
+  const yRange:[number,number] = <[number,number]>d3.extent(extentList)
+  // const yRange:[number,number] = d3.extent(stockArray.reduce<number[]>((acc,element) => {
+  //   const thisExtent = [element.min,element.max]
+  //   return [...acc,...thisExtent]
+  // },[]))
   return yRange
 }
 
